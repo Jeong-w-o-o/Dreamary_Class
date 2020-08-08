f2707d40b1525ef0831dc8f6be20c15287719aff
Jeong-w-o-o <irmnels@gmail.com>
2020-08-09 00:00:19 +0900

    second commit

--- page/views.py
+++ page/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html') # 홈에서는 요청 (request)이 들어오면 요청과 함께 home.html을 돌려줌
 
+def introduce(request):
+    return render(request, 'introduce.html')
